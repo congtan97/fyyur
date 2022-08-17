64a0cdff4a3e451c755ba541730947500aeb65bf
congtan97 <congtan98@gmail.com>
2022-08-17 21:15:55 +0700

    using environment variables values

--- config.py
+++ config.py
@@ -7,7 +7,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'admin')
+DB_NAME = os.getenv('DB_NAME', 'fyyur')
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/fyyur'
+DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/fyyur'
+
+SQLALCHEMY_DATABASE_URI = DB_PATH
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
